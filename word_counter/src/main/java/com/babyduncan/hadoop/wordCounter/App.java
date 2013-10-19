6e1daa35032f029fe3a454afab8778810f4eba08
guohaozhao <babyduncan@qq.com>
2013-10-19 16:21:19 +0800

    init java project

---
+++ word_counter/src/main/java/com/babyduncan/hadoop/wordCounter/App.java
@@ -0,0 +1,13 @@
+package com.babyduncan.hadoop.wordCounter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
